772fe2124100dc1d9ea4340cfb718741e4564946
grtvd <grtvd@users.noreply.github.com>
2019-09-27 11:35:06 -0400

    Updated deploy.sh to use latest version of appengine-java-sdk.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/projects/appengine-java-sdk-1.9.63/bin/appcfg.sh update ~/projects/rightingcode/out/artifacts/rightingcode
+~/projects/appengine-java-sdk-1.9.76/bin/appcfg.sh update ~/projects/rightingcode/out/artifacts/rightingcode
